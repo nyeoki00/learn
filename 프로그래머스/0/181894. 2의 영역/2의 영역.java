f61b1f2c3ba43712d33e4ed661e051ac7651bfe0
nyeoki <147301293+nyeoki00@users.noreply.github.com>
2024-11-17 23:44:01 +0900

    [level 0] Title: 2의 영역, Time: 0.88 ms, Memory: 86.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/181894. 2의 영역/2의 영역.java
@@ -0,0 +1,36 @@
+import java.util.Arrays;
+
+class Solution {
+    public int[] solution(int[] arr) {
+        int[] answer = {};
+        
+        int startIdx = -1;
+        int endIdx = -1;
+        
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == 2) {
+                startIdx = i;
+                break;
+            }
+        }
+        
+        if (startIdx != -1) {
+            for (int i = arr.length - 1; i >= 0; i--) {
+                if (arr[i] == 2) {
+                    endIdx = i;
+                    break;
+                }
+            }
+        }
+        
+        if (startIdx == -1) {
+            answer = new int[] {-1};
+        } else if (startIdx == endIdx) {
+            answer = new int[] {arr[startIdx]};
+        } else if (startIdx != endIdx) {
+            answer = Arrays.copyOfRange(arr, startIdx, endIdx + 1);
+        }
+        
+        return answer;
+    }
+}
